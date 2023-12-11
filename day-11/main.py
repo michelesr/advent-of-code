6036e0c5569e534b0b2db51e08d94b41314bf330
Michele Sorcinelli <michelesr@autistici.org>
2023-12-11 15:46:21 +0000

    Refactor to improve peformance
    Compute the empty rows and cols just once to save lots of time.

--- day-11/main.py
+++ day-11/main.py
@@ -10,14 +10,6 @@ def get_col(i):
     return [row[i] for row in grid]
 
 
-def get_empty_rows():
-    return [i for i in range(len(grid)) if "#" not in get_row(i)]
-
-
-def get_empty_cols():
-    return [i for i in range(len(grid[0])) if "#" not in get_col(i)]
-
-
 def print_grid():
     for row in grid:
         print("".join(row))
@@ -33,9 +25,9 @@ def find_galaxies():
 
 
 def expand():
-    for i in reversed(get_empty_rows()):
+    for i in reversed(EMPTY_ROWS):
         grid.insert(i, copy(grid[i]))
-    for j in reversed(get_empty_cols()):
+    for j in reversed(EMPTY_COLS):
         for row in grid:
             row.insert(j, row[j])
 
@@ -46,14 +38,14 @@ def distance(a, b):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
-def super_distance(a, b, multiplier=1000000):
+def distance2(a, b, multiplier=1000000):
     y1, x1 = a
     y2, x2 = b
-    empty_rows = [i for i in get_empty_rows() if i in range(y1, y2)]
+    empty_rows = [i for i in EMPTY_ROWS if i in range(y1, y2)]
     step = 1
     if x2 < x1:
         step = -1
-    empty_cols = [i for i in get_empty_cols() if i in range(x1, x2, step)]
+    empty_cols = [i for i in EMPTY_COLS if i in range(x1, x2, step)]
 
     dy = abs(y1 - y2) + (len(empty_rows) * (multiplier - 1))
     dx = abs(x1 - x2) + (len(empty_cols) * (multiplier - 1))
@@ -63,11 +55,13 @@ def super_distance(a, b, multiplier=1000000):
 with open("./input") as f:
     grid = [list(line.strip()) for line in f.readlines()]
 
+EMPTY_ROWS = [i for i in range(len(grid)) if "#" not in get_row(i)]
+EMPTY_COLS = [i for i in range(len(grid[0])) if "#" not in get_col(i)]
+
 old_grid = deepcopy(grid)
+
 expand()
 print(sum((distance(a, b) for a, b in combinations(find_galaxies(), 2))))
 
 grid = old_grid
-
-# 35s with CPython, 7s with PyPy
-print(sum((super_distance(a, b) for a, b in combinations(find_galaxies(), 2))))
+print(sum((distance2(a, b) for a, b in combinations(find_galaxies(), 2))))
