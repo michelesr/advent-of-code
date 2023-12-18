2b410090bc6711699ed2d9728ebd000b6754e812
Michele Sorcinelli <michelesr@autistici.org>
2023-12-18 14:58:37 +0000

    Add day-18 with part 2

--- day-18/main.py
+++ day-18/main.py
@@ -1,49 +1,45 @@
-from matplotlib.path import Path
+from shapely import Polygon
 from sys import argv
+
+def find_points(lines: list[tuple[str, int, str]], part_two: bool = False) -> list[tuple[int, int]]:
+    points: list[tuple[int, int]] = [(0, 0)]
+    x, y = 0, 0
+    for line in lines:
+        d, n = line[0], line[1]
+        if part_two:
+            h = line[2]
+            d = int(h[-1])
+            n = int(h[1:-1], 16)
+        match d:
+            case "L" | 2:
+                x -= n
+            case "R" | 0:
+                x += n
+            case "U" | 3:
+                y += n
+            case "D" | 1:
+                y -= n
+        points.append((x, y))
+    return points
+
+
+def score(points: list[tuple[int, int]]) -> int:
+    # Pick's theorem + Shoelace formula
+    #
+    # A is the area of the figure
+    # b is the perimeter
+    # i is the number of integer points in the perimeter
+    #
+    # i + b = A + b/2 + 1
+    p = Polygon(points)
+    A = p.area
+    b = p.length
+    res = A + b / 2 + 1
+    return int(res)
+
 
 lines = [line.strip().split(" ") for line in open(argv[1])]
 lines = [(x[0], int(x[1]), x[2].replace("(", "").replace(")", "")) for x in lines]
 
-holes: list[tuple[int, int]] = [(0, 0)]
-r, c = 0, 0
-
-for line in lines:
-    match line[0]:
-        case "L":
-            for i in range(c - 1, c - line[1] - 1, -1):
-                c = i
-                if (r, c) not in holes:
-                    holes.append((r, c))
-        case "R":
-            for i in range(c + 1, c + line[1] + 1, 1):
-                c = i
-                if (r, c) not in holes:
-                    holes.append((r, c))
-        case "U":
-            for i in range(r - 1, r - line[1] - 1, -1):
-                r = i
-                if (r, c) not in holes:
-                    holes.append((r, c))
-        case "D":
-            for i in range(r + 1, r + line[1] + 1, 1):
-                r = i
-                if (r, c) not in holes:
-                    holes.append((r, c))
-
-print(len(holes))
-
-MIN_I = min([x[0] for x in holes])
-MAX_I = max([x[0] for x in holes])
-
-MIN_J = min([x[1] for x in holes])
-MAX_J = max([x[1] for x in holes])
-
-p = Path(holes)
-
-res = 0
-for i in range(MIN_I, MAX_I + 1):
-    for j in range(MIN_J, MAX_J + 1):
-        if (i, j) in holes or p.contains_point((i, j)):
-            res += 1
-
-print(res)
+print(score(find_points(lines)))
+print(score(find_points(lines, part_two=True)))
