6aa5ee4b8e302365fdfe39131e6b2384899b01bd
Michele Sorcinelli <michelesr@autistici.org>
2024-12-01 15:21:20 +0000

    Refactor 2024/day-1

--- 2024/day-1/main.py
+++ 2024/day-1/main.py
@@ -8,23 +8,8 @@ with open(getenv("AOC_INPUT", "example_input.txt"), "r") as f:
         lists[0].append(int(parts[0]))
         lists[1].append(int(parts[3].strip()))
 
-s_lists = (sorted(lists[0]), sorted(lists[1]))
-res = 0
-
-while len(s_lists[0]) > 0:
-    a = min(s_lists[0])
-    b = min(s_lists[1])
-    s_lists[0].remove(a)
-    s_lists[1].remove(b)
-    d = abs(a - b)
-    res += d
-
-print(res)
+lists = (sorted(lists[0]), sorted(lists[1]))
+print(sum([abs(a - b) for a, b in zip(*lists)]))
 
 counters = (Counter(lists[0]), Counter(lists[1]))
-
-res = 0
-for n, count in counters[0].items():
-    res += n * count * counters[1][n]
-
-print(res)
+print(sum([n * count * counters[1][n] for n, count in counters[0].items()]))
