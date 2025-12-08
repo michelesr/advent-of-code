54e8ec708aaeb9a59b9a7aa864199e0fbf68f3f1
Michele Sorcinelli <michelesr@autistici.org>
2025-12-08 12:50:34 +0000

    Refactor and type annotate day-8 2025

--- 2025/day-8/main.py
+++ 2025/day-8/main.py
@@ -1,25 +1,39 @@
-from os import getenv
 from collections import Counter
-from math import sqrt, prod
+from math import prod, sqrt
+from os import getenv
+from typing import cast
+
+Box = tuple[int, int, int]
 
 
-def distance(a, b):
+def distance(a: Box, b: Box) -> float:
     a1, a2, a3 = a
     b1, b2, b3 = b
     return sqrt((a1 - b1) ** 2 + (a2 - b2) ** 2 + (a3 - b3) ** 2)
 
 
+def connect_pair(circuits: dict[Box, set[Box]], pair: tuple[Box, Box]) -> set[Box]:
+    a, b = pair
+    if circuits[a] is circuits[b]:
+        return circuits[a]
+    new_circuit = circuits[a] | circuits[b]
+    for box in new_circuit:
+        circuits[box] = new_circuit
+    return new_circuit
+
+
 with open(getenv("AOC_INPUT", "example_input.txt"), "r") as f:
-    boxes = [tuple(int(n) for n in line.strip().split(",")) for line in f.readlines()]
+    boxes = [
+        cast(Box, tuple(int(n) for n in line.strip().split(",")))
+        for line in f.readlines()
+    ]
 
-distances = {}
-
+distances: dict[tuple[Box, Box], float] = {}
 for box in boxes:
     for other in set(boxes) - {box}:
         a, b = sorted([box, other])
         if (a, b) not in distances:
             distances[(a, b)] = distance(a, b)
-
 
 # part one
 pairs = sorted(distances.items(), key=lambda item: item[1], reverse=True)
@@ -30,25 +44,16 @@ if len(boxes) >= 100:
     LIMIT = 1000
 
 for i in range(LIMIT):
-    a, b = pairs.pop()[0]
-    new_circuit = circuits[a] | circuits[b]
-    for box in new_circuit:
-        circuits[box] = new_circuit
+    connect_pair(circuits, pairs.pop()[0])
 
 c = Counter(map(tuple, circuits.values()))
 print(prod(list(sorted(c.values(), reverse=True))[:3]))
 
 # part two
-pairs = sorted(distances.items(), key=lambda item: item[1], reverse=True)
-circuits = {box: {box} for box in boxes}
-
 L = len(boxes)
-
 while pairs:
-    a, b = pairs.pop()[0]
-    new_circuit = circuits[a] | circuits[b]
-    for box in new_circuit:
-        circuits[box] = new_circuit
-    if len(new_circuit) == L:
+    pair = pairs.pop()[0]
+    if len(connect_pair(circuits, pair)) == L:
+        a, b = pair
         print(a[0] * b[0])
         break
